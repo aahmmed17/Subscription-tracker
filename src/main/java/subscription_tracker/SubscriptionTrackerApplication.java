ae0ac8399bb6430c469d7c6bb0bb301d139a0d4e
Aftab <aftabahmmed79@gmail.com>
2026-08-14 17:34:09 +0100

    Intial Spring Boot project scaffold with PostSQL connection

---
+++ src/main/java/subscription_tracker/SubscriptionTrackerApplication.java
@@ -0,0 +1,13 @@
+package subscription_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionTrackerApplication.class, args);
+	}
+
+}
